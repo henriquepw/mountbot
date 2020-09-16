8b3eaaa20c1013a63b729e6c0c1b728036878295
Henrique Miranda <enrque.ns@gmail.com>
2020-09-15 23:23:51 -0300

    refactor: apply lint

--- packages/chat-block/react/Chat.tsx
+++ packages/chat-block/react/Chat.tsx
@@ -1,25 +1,24 @@
 import React from 'react'
 
-
-
-
-
 interface ChatProps { }
 
-const Chat: StorefrontFunctionComponent<ChatProps> = ({ }) => {
-
+const Chat: StorefrontFunctionComponent<ChatProps> = () => {
   return (
     <div style={{
       position: 'absolute',
       right: '32px',
       bottom: '64px',
-    
     }}>
-
-      <iframe src="https://d1ynr93g6z78ed.cloudfront.net/index.html" width="500px" height="500px" style={{    borderColor: '#E76F51',
-      borderWidth: '2px'}}></iframe>
+      <iframe
+        src="https://d1ynr93g6z78ed.cloudfront.net/index.html" 
+        width="400px" 
+        height="500px" 
+        style={{
+          borderColor: '#E76F51',
+          borderWidth: '2px',
+        }}
+      ></iframe>
     </div>
-  
   )
 }
 
