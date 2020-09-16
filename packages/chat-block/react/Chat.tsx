d23998171fecb3768876eaa41f57762c2631e550
italoadler <adler.italo@gmail.com>
2020-09-15 23:01:43 -0300

    Positioning the chat window

--- packages/chat-block/react/Chat.tsx
+++ packages/chat-block/react/Chat.tsx
@@ -1,12 +1,23 @@
 import React from 'react'
 
+
+
+
+
 interface ChatProps { }
 
 const Chat: StorefrontFunctionComponent<ChatProps> = ({ }) => {
+
   return (
-  <>
-    <iframe src="https://d1ynr93g6z78ed.cloudfront.net/index.html" width="500px" height="500px"></iframe>
-  </>
+    <div style={{
+      position: 'absolute',
+      right: '32px',
+      bottom: '32px', 
+    }}>
+
+      <iframe src="https://d1ynr93g6z78ed.cloudfront.net/index.html" width="500px" height="500px" ></iframe>
+    </div>
+  
   )
 }
 
